645b21d8685d26a50874dc378e8a9152585d5c1f
sivagithub2019 <rskr74@yahoo.com>
2024-08-02 15:42:35 +0100

    added test class

---
+++ src/test/java/com/ktree/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ktree.helloworld;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class HelloworldApplicationTests {
+  @Test
+  public void testHelloWorld() {
+    HelloworldApplicationTests service = new HelloworldApplicationTests();
+    assertEquals("Hello, world!", "Hello, world!");
+  }
+
+}
